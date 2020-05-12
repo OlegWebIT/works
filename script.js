d4ff71716f8b003ed98d78ee211636a76801fa31
Oleg <krugliansky54@gmail.com>
2020-05-12 12:52:54 +0300

    added new alerts

--- script.js
+++ script.js
@@ -1,21 +1,79 @@
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
 
-const personalMovieDB = {
+function start () {
+	numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+		numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+	}
+}
+
+start();
+
+
+
+let personalMovieDB = {
 	count: numberOfFilms,
 	movies: {},
 	actors: {},
 	genres: [],
-	privat: false
+	privat: true
 };
 
-const a = prompt('Один из последних фильмов',''),
-	  b = prompt('На сколько оцените его?',''),
-	  c = prompt('Один из последних фильмов',''),
-	  d = prompt('На сколько оцените его?','');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+function rememberMyFilms() {
 
-console.log(personalMovieDB);
+	for (let i = 0; i < 2; i++) {
+		const a = prompt('Один из последних фильмов'),
+			  b = prompt('На сколько оцените его?');
+		personalMovieDB.movies [a] = b;
+
+		 if (a != null && b != null && a !='' && b != '' && a.length < 50){
+			 console.log('done');
+		 } else {
+			 console.log('Eror');
+			 i--;
+		 }
+	}
+
+}
+
+rememberMyFilms();
+
+
+function detectPersonalLevel() {
+	if (numberOfFilms < 10) {
+		alert('Просмотрено довольно мало фильмов!');
+	} else if (numberOfFilms >= 10 && numberOfFilms <= 30) {
+		alert('Вы классический зритель!');
+	} else if (numberOfFilms > 30) {
+		alert('Вы киноман!');
+	} else {
+		alert('Произошла ошибка :(');
+	}
+}
+detectPersonalLevel();
+
+function showMyDB () {
+	if (personalMovieDB.privat != 'true') {
+		console.log(personalMovieDB);
+	} else if (personalMovieDB.privat == 'true') {
+
+	} else {
+
+	}
+}
+
+showMyDB();
+
+
+function wirteYourGenres() {
+	for (let i = 1; i <= 3; i++) {
+		personalMovieDB.genres [i - 1] = prompt('Ваш любимый жанр под номером?');	
+	}
+
+}
+
+wirteYourGenres ();
